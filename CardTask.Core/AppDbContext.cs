85a0914cdcef5009c6ecac01143da360612441d3
AubreyL05 <151494001+AubreyL05@users.noreply.github.com>
2026-06-05 16:18:48 -0400

    SCRUM-15/16/17: Relational database pipeline and localdb authentication locked

---
+++ CardTask.Core/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CardTask.Core.Models;
+
+namespace CardTask.Core;
+
+sealed public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    // Note: You will add Tasks and Courses collections here in Sprint 2!
+}
---
+++ CardTask.Core/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CardTask.Core.Models;
+
+sealed public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
